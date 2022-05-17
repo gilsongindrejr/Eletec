341fa83d98a91ba4ac14c7e324055dfe71896030
Gilson <gindrejunior@hotmail.com>
2022-05-17 16:24:53 -0300

    returned setter methods and new constructor

--- src/models/Equipamento.java
+++ src/models/Equipamento.java
@@ -5,7 +5,9 @@ public class Equipamento {
     private String modelo;
     private String tamanhoDaTela;
 
-    public Equipamento(String marca, String modelo, String tamanhoDaTela){
+    public Equipamento(){}
+
+    public Equipamento(String marca, String modelo, String tamanhoDaTela) {
         this.marca = marca;
         this.modelo = modelo;
         this.tamanhoDaTela = tamanhoDaTela;
@@ -15,12 +17,23 @@ public class Equipamento {
         return this.marca;
     }
 
+    public void setMarca(String marca){
+        this.marca = marca;
+    }
+
     public String getModelo(){
         return this.modelo;
+    }
+
+    public void setModelo(String modelo){
+        this.modelo = modelo;
     }
 
     public String getTamanhoDaTela(){
         return this.tamanhoDaTela;
     }
 
+    public void setTamanhoDaTela(String tamanhoDaTela){
+        this.tamanhoDaTela = tamanhoDaTela;
+    }
 }

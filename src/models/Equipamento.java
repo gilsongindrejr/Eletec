e9acec3741eb7fe19620ef0de7fbdce497a0fb2e
Gilson <gindrejunior@hotmail.com>
2022-05-16 23:04:43 -0300

    removed nome attr and set methods

--- src/models/Equipamento.java
+++ src/models/Equipamento.java
@@ -2,47 +2,26 @@ package models;
 
 public class Equipamento {
 
-    private String nome;
-    private String marca;
-    private String modelo;
-    private String tamanhoDaTela;
+    private final String marca;
+    private final String modelo;
+    private final String tamanhoDaTela;
 
-    public Equipamento(String nome, String marca, String modelo, String tamanhoDaTela){
-        this.nome = nome;
+    public Equipamento(String marca, String modelo, String tamanhoDaTela){
         this.marca = marca;
         this.modelo = modelo;
         this.tamanhoDaTela = tamanhoDaTela;
-    }
-
-    public String getNome(){
-        return this.nome;
-    }
-
-    public void setNome(String nome){
-        this.nome = nome;
     }
 
     public String getMarca(){
         return this.marca;
     }
 
-    public void setMarca(String marca){
-        this.marca = marca;
-    }
-
     public String getModelo(){
         return this.modelo;
-    }
-
-    public void setModelo(String modelo){
-        this.modelo = modelo;
     }
 
     public String getTamanhoDaTela(){
         return this.tamanhoDaTela;
     }
 
-    public void setTamanhoDaTela(String tamanhoDaTela){
-        this.tamanhoDaTela = tamanhoDaTela;
-    }
 }
